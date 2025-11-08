c286887331766670bf3bbc9e2234ec65827b9d53
keyur <keyurkulkarni66@gmail.com>
2025-11-08 18:33:41 +0530

    single user website is almost done

--- remu/src/App.jsx
+++ remu/src/App.jsx
@@ -6,11 +6,32 @@ import ma from "./assets/ma.mp3";
 import pa from "./assets/pa.mp3";
 import dha from "./assets/dha.mp3";
 import ni from "./assets/ni.mp3";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { motion } from "framer-motion";
+
 function App() {
+  const showScoreRef = useRef();
   const swara = { sa, re, ga, ma, pa, dha, ni };
-  let seq = [];
+
+  // const [clicksubmit, setclicksubmit] = useState(0);
+  const [notes, setnotes] = useState("");
+  const [seq, setseq] = useState([]);
+  const [clicksubmit, setclicksubmit] = useState(0);
+  const [clickvs, setclickvs] = useState(0);
+
+  const [clickedvs, setclickedvs] = useState(0);
+
+  const [score, setscore] = useState(0);
+  const [messe, setmesse] = useState("");
+  const [iddd, setiddd] = useState("");
+  let dusc = 0;
   let func = () => {
-    seq = [];
+    let se = [];
+    setclicksubmit(1);
+    setclickvs(0);
+    setclickedvs(0);
     let para = document.getElementById("t").value;
     for (let i = 0; i < para.length; i++) {
       if (para[i] == "s") {
@@ -18,7 +39,8 @@ function App() {
           i++;
         }
         if (i < para.length) {
-          seq.push("sa");
+          se.push("sa");
+          dusc += 2;
         } else {
           break;
         }
@@ -27,7 +49,8 @@ function App() {
           i++;
         }
         if (i < para.length) {
-          seq.push("re");
+          se.push("re");
+          dusc += 2;
         } else {
           break;
         }
@@ -36,7 +59,8 @@ function App() {
           i++;
         }
         if (i < para.length) {
-          seq.push("ga");
+          se.push("ga");
+          dusc += 2;
         } else {
           break;
         }
@@ -45,7 +69,8 @@ function App() {
           i++;
         }
         if (i < para.length) {
-          seq.push("ma");
+          se.push("ma");
+          dusc += 2;
         } else {
           break;
         }
@@ -54,7 +79,8 @@ function App() {
           i++;
         }
         if (i < para.length) {
-          seq.push("pa");
+          se.push("pa");
+          dusc += 2;
         } else {
           break;
         }
@@ -67,7 +93,8 @@ function App() {
             i++;
           }
           if (i < para.length) {
-            seq.push("dha");
+            se.push("dha");
+            dusc += 3;
           } else {
             break;
           }
@@ -79,29 +106,93 @@ function App() {
           i++;
         }
         if (i < para.length) {
-          seq.push("ni");
+          se.push("ni");
+          dusc += 2;
         } else {
           break;
         }
       }
     }
-    let delay = 0;
-    for (let i = 0; i < seq.length; i++) {
-      console.log(seq[i]);
-      let swar = swara[seq[i]];
+
+    setseq(se);
+
+    if (se.length == 0) {
+      alert("your paragraph is not at all musical bro!!");
+    }
+    let k = "";
+    for (let i = 0; i < se.length; i++) {
+      k += se[i];
+      k += " ";
+    }
+    setnotes(k);
+
+    let delay = 500; // 1 second delay before the first note
+    for (let i = 0; i < se.length; i++) {
+      let swar = swara[se[i]];
       setTimeout(() => {
         new Audio(swar).play();
       }, delay);
-      delay += 335;
+      delay += 500; // 0.5s note + 1s gap
+    }
+    let kk = Math.round((dusc * 10) / para.length);
+    setscore(kk);
+
+    if (kk < 5) {
+      setmesse("Needs rhythm");
+      setiddd("score-low");
+    } else if (kk >= 5 && kk < 9) {
+      setmesse("Nice flow!");
+      setiddd("score-mid");
+    } else if (kk >= 9) {
+      setmesse("Beautiful cadence");
+      setiddd("score-high");
     }
   };
+  const handleViewScore = () => {
+    setclickvs(1);
+    setclickedvs(1);
+  };
+  useEffect(() => {
+    if (clickedvs === 1) {
+      showScoreRef.current.scrollIntoView({ behavior: "smooth" });
+      setclickedvs(0);
+    }
+  }, [clickedvs]);
   return (
-    <>
+    <div id="body">
+      <p id="name">ParaTune</p>
+      <p id="tagline">see how musical your paragraph is!!</p>
       <div id="notes">
-        <input type="text" id="t" style={{ texttransform: "lowercase" }} />
+        <textarea
+          type="text"
+          id="t"
+          onChange={(e) => {
+            e.target.value = e.target.value.toLowerCase();
+          }}
+          autoFocus={true}
+          required={true}
+          placeholder="Enter your paragraph here..."
+        />
         <input type="submit" id="su" onClick={func} />
       </div>
-    </>
+
+      {seq.length > 0 && clicksubmit === 1 && (
+        <div id="show-seq">
+          <div id="noteis">
+            <p id="note">{notes}</p>
+          </div>
+          <button id="view-score" onClick={handleViewScore}>
+            View Score
+          </button>
+        </div>
+      )}
+      {clickvs === 1 && (
+        <div id="show-score" className={iddd} ref={showScoreRef}>
+          <p id="scoore">{score}/10</p>
+          <p id="message">{messe}</p>
+        </div>
+      )}
+    </div>
   );
 }
 
